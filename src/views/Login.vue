7d30895a28237427762085bdeb42fa57f17b8c86
Val <v.zaviriukha@gmail.com>
2020-07-13 14:11:10 +0300

    - start commit

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,11 +1,13 @@
 <template>
-    <form class="card auth-card">
+    <form class="card auth-card" @submit.prevent="submitHandler">
         <div class="card-content">
             <span class="card-title">Домашняя бухгалтерия</span>
             <div class="input-field">
                 <input
                         id="email"
                         type="text"
+                        v-model.trim="email"
+                        :class="{}"
                 >
                 <label for="email">Email</label>
                 <small class="helper-text invalid">Email</small>
@@ -14,26 +16,11 @@
                 <input
                         id="password"
                         type="password"
-                        class="validate"
+                        v-model="password"
                 >
                 <label for="password">Пароль</label>
                 <small class="helper-text invalid">Password</small>
             </div>
-            <div class="input-field">
-                <input
-                        id="name"
-                        type="text"
-                        class="validate"
-                >
-                <label for="name">Имя</label>
-                <small class="helper-text invalid">Name</small>
-            </div>
-            <p>
-                <label>
-                    <input type="checkbox"/>
-                    <span>С правилами согласен</span>
-                </label>
-            </p>
         </div>
         <div class="card-action">
             <div>
@@ -41,25 +28,40 @@
                         class="btn waves-effect waves-light auth-submit"
                         type="submit"
                 >
-                    Зарегистрироваться
+                    Войти
                     <i class="material-icons right">send</i>
                 </button>
             </div>
 
             <p class="center">
-                Уже есть аккаунт?
-                <a href="/">Войти!</a>
+                Нет аккаунта?
+                <router-link to="/register">Зарегистрироваться</router-link>
             </p>
         </div>
     </form>
 </template>
 
 <script>
+    import {email, required, minLength} from 'vuelidate/lib/validators'
+
     export default {
-        name: "Login"
+        data: () => ({
+            email: '',
+            password: ''
+        }),
+        validations:{
+            email: {
+                email, required
+            },
+            password: {
+                required, minLength: minLength(6)
+            }
+        },
+        methods: {
+            submitHandler() {
+                this.$router.push('/')
+            }
+        },
+
     }
 </script>
-
-<style scoped>
-
-</style>
