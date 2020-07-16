c85c5906f6a2d300fd300561fa5efa7a1055aa45
Val <v.zaviriukha@gmail.com>
2020-07-16 15:31:35 +0300

    - Loader.vue

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import router from './router'
 import store from './store'
 import dateFilter from "./filters/date.filter";
 import messagePlugin from "./utils/message.plugin"
+import Loader from "./components/app/Loader"
 import './registerServiceWorker'
 import 'materialize-css/dist/js/materialize.min'
 
@@ -17,6 +18,7 @@ Vue.config.productionTip = false
 Vue.use(Vuelidate)
 Vue.use(messagePlugin)
 Vue.filter('date', dateFilter)
+Vue.component('Loader', Loader)
 
 
 firebase.initializeApp({
