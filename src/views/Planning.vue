8f47a1fe1aaef93ee17833d75390305e93901493
Val <v.zaviriukha@gmail.com>
2020-07-24 13:15:40 +0300

    - planing

--- src/views/Planning.vue
+++ src/views/Planning.vue
@@ -2,19 +2,26 @@
     <div>
         <div class="page-title">
             <h3>Планирование</h3>
-            <h4>12 212</h4>
+            <h4>{{info.bill | currency('UAH')}}</h4>
         </div>
 
-        <section>
-            <div>
+        <Loader v-if="loading"/>
+
+        <p v-else-if="!categories.length" class="center">Категорий пока нет.
+            <router-link to="/categories">Добавить категорию</router-link>
+        </p>
+
+        <section v-else>
+            <div v-for="cat of categories" :key="cat.id">
                 <p>
-                    <strong>Девушка:</strong>
-                    12 122 из 14 0000
+                    <strong>{{cat.title}}</strong>
+                    {{cat.spend | currency}} из {{cat.limit | currency}}
                 </p>
-                <div class="progress">
+                <div class="progress" v-tooltip="cat.tooltip">
                     <div
-                            class="determinate green"
-                            style="width:40%"
+                            class="determinate"
+                            :class="[cat.progressColor]"
+                            :style="{width: cat.progressPercent + '%'}"
                     ></div>
                 </div>
             </div>
@@ -23,8 +30,52 @@
 </template>
 
 <script>
+    import {mapGetters} from 'vuex'
+    import currencyFilter from "../filters/currencyFilter";
+
     export default {
-        name: "Planning"
+        name: "Planning",
+        data: () => ({
+            loading: true,
+            categories: []
+        }),
+        computed: {
+            ...mapGetters(['info'])
+        },
+        async mounted() {
+            const records = await this.$store.dispatch('fetchRecords')
+            const categories = await this.$store.dispatch('fetchCategories')
+
+            this.categories = categories.map(cat => {
+                const spend = records
+                    .filter(r => r.categoryId === cat.id)
+                    .filter(r => r.type === 'outcome')
+                    .reduce((total, record) => {
+                        return total += +record.amount
+                    }, 0)
+
+                const percent = 100 * spend / cat.limit
+                const progressPercent = percent > 100 ? 100 : percent
+                const progressColor = percent < 60
+                    ? 'green'
+                    : percent < 100
+                        ? 'yellow'
+                        : 'red'
+
+                const tooltipValue = cat.limit - spend
+                const tooltip = `${tooltipValue < 0 ? 'Превышение на' : 'Осталось'} ${currencyFilter(Math.abs(tooltipValue))}`
+
+                return {
+                    ...cat,
+                    progressPercent,
+                    progressColor,
+                    spend,
+                    tooltip
+                }
+            })
+
+            this.loading = false
+        }
     }
 </script>
 
