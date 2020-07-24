b0f20acc47ceb2a7a7276ff37abac03853996dc6
Val <v.zaviriukha@gmail.com>
2020-07-24 13:39:51 +0300

    - router protect

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import firebase from "firebase/app";
 
 Vue.use(VueRouter)
 
@@ -20,7 +21,7 @@ const routes = [
     {
         path: '/',
         name: 'home',
-        meta: {layout: 'main'},
+        meta: {layout: 'main', auth: true},
         component: () => import('../views/Home.vue')
     },
     {
@@ -32,31 +33,31 @@ const routes = [
     {
         path: '/detail/:id',
         name: 'detail',
-        meta: {layout: 'main'},
+        meta: {layout: 'main', auth: true},
         component: () => import('../views/Detail.vue')
     },
     {
         path: '/history',
         name: 'history',
-        meta: {layout: 'main'},
+        meta: {layout: 'main', auth: true},
         component: () => import('../views/History.vue')
     },
     {
         path: '/planning',
         name: 'planning',
-        meta: {layout: 'main'},
+        meta: {layout: 'main', auth: true},
         component: () => import('../views/Planning.vue')
     },
     {
         path: '/profile',
         name: 'profile',
-        meta: {layout: 'main'},
+        meta: {layout: 'main', auth: true},
         component: () => import('../views/Profile.vue')
     },
     {
         path: '/record',
         name: 'record',
-        meta: {layout: 'main'},
+        meta: {layout: 'main', auth: true},
         component: () => import('../views/Record.vue')
     }
 ]
@@ -65,6 +66,17 @@ const router = new VueRouter({
     mode: 'history',
     base: process.env.BASE_URL,
     routes
+})
+
+router.beforeEach((to, from, next) => {
+    const currentUser = firebase.auth().currentUser
+    const requireAuth = to.matched.some(record => record.meta.auth)
+
+    if (requireAuth && !currentUser) {
+        next('/login?message=login')
+    } else {
+        next()
+    }
 })
 
 export default router
--- src/utils/messages.js
+++ src/utils/messages.js
@@ -2,5 +2,6 @@ export default {
     'logout' : 'Вы вышли из системы',
     'auth/user-not-found' : 'Пользователь не найден',
     'auth/wrong-password': 'Не верный пароль',
-    'auth/email-already-in-use' : 'Пользователь с таким email уже зарегистрирован'
+    'auth/email-already-in-use' : 'Пользователь с таким email уже зарегистрирован',
+    'login': 'Войдите в систему'
 }
