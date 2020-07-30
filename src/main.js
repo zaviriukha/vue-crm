72da9ac6fb2482381b863d85afd61b9e4ddc5ee0
Val <v.zaviriukha@gmail.com>
2020-07-30 15:46:21 +0300

    - pagination

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ import messagePlugin from "./utils/message.plugin"
 import Loader from "./components/app/Loader"
 import './registerServiceWorker'
 import 'materialize-css/dist/js/materialize.min'
+import Paginate from 'vuejs-paginate'
 
 import firebase from 'firebase/app'
 import 'firebase/auth'
@@ -23,6 +24,7 @@ Vue.filter('date', dateFilter)
 Vue.filter('currency', currencyFilter)
 Vue.directive('tooltip', tooltipDirective)
 Vue.component('Loader', Loader)
+Vue.component('paginate', Paginate)
 
 
 firebase.initializeApp({
--- src/views/History.vue
+++ src/views/History.vue
@@ -13,36 +13,52 @@
         <p class="center" v-else-if="!records.length">Записей нет <router-link to="/record"> Добавте первую</router-link> </p>
 
         <section v-else>
-            <HistoryTable :records="records"/>
+            <HistoryTable :records="items"/>
+
+            <Paginate
+                    v-model="page"
+                    :page-count="pageCount"
+                    :click-handler="pageChangeHandler"
+                    :prev-text="'Назад'"
+                    :next-text="'Вперед'"
+                    :container-class="'pagination'"
+                    :page-class="'waves-effect'"
+            />
         </section>
     </div>
 </template>
 
 <script>
     import HistoryTable from "../components/HistoryTable";
+    import Paginate from 'vuejs-paginate'
+    import paginationMixin from '../mixins/mixins.pagination'
 
     export default {
         name: "History",
-        components: {HistoryTable},
+        mixins: [paginationMixin],
+        components: {HistoryTable, Paginate},
         data: () => ({
             loading: true,
             records: [],
-            categories: []
         }),
         async mounted() {
-            // this.records = await this.$store.dispatch('fetchRecords')
-            const records = await this.$store.dispatch('fetchRecords')
-            this.categories = await this.$store.dispatch('fetchCategories')
-            this.records = records.map(record => {
+            this.records = await this.$store.dispatch('fetchRecords')
+            const categories = await this.$store.dispatch('fetchCategories')
+            this.setupPagination(this.records.map(record => {
                 return {
                     ...record,
-                    categoryName: this.categories.find(c => c.id === record.categoryId).title,
+                    categoryName: categories.find(c => c.id === record.categoryId).title,
                     typeClass: record.type === 'income' ? 'green' : 'red',
                     typeText: record.type === 'income' ? 'Доход' : 'Расход',
                 }
-            })
+            }))
 
             this.loading = false
+        },
+        methods: {
+            pageChangerHandler() {
+
+            }
         }
     }
 </script>
